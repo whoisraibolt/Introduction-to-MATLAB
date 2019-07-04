f7b558f09f079ff758ab3f777af77fc16352a80e
Alexandra Miguel Raibolt da Silva <alexandra.raibolt@gmail.com>
2019-07-04 19:06:35 -0300

    Upload exercise_01.m

---
+++ exercise_01.m
@@ -0,0 +1,6 @@
+clc;                    % Clear Command Window %
+clear all;              % Remove items from workspace, freeing up system memory %
+warning off;            % Disable all warnings %
+
+mkdir MATLAB
+cd MATLAB
